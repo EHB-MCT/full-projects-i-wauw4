8baac83bd62785ad2dd3b9b9a40c1e7637314934
alexander <alexander.mersseman2003@gmail.com>
2022-01-19 17:13:21 +0100

    verder gewerkt

--- js/index.js
+++ js/index.js
@@ -1,1 +1,41 @@
+"use strict"
 
+function overheid() {
+    document.getElementById('overheid').innerHTML = 'Brussels mobiliteit, Vlaamse Overheid departement omgeving, Gemeente Dilbeek, leefmilieu Brussel'
+}
+
+function bedrijven() {
+    document.getElementById('bedrijven').innerHTML = 'Zaventem Airport'
+}
+
+function organisaties() {
+    document.getElementById('organisaties').innerHTML = 'VITO, UGENT, Gezinsbond, Leuven Mindgate, SOM, Havanza, Koren, Carki, recyclart, COOP, Pronatura, Arib, TADA'
+}
+
+function festival() {
+    document.getElementById('festival').innerHTML = 'Vijverfestival, Periferiafestival, StormOpKomst, Spot On Festival, Door to Door, Fablab, and& festival, How to Best Practice'
+}
+
+function cultuur() {
+    document.getElementById('cultuur').innerHTML = 'Bozar, CC Westrand, Z33, DeSingel'
+}
+
+function centrum() {
+    document.getElementById('centrum').innerHTML = 'Essegem, Destelheide, Muntpunt'
+}
+
+function instelling() {
+    document.getElementById('instelling').innerHTML = 'VAI, AWB, CIVA'
+}
+
+function scholen() {
+    document.getElementById('scholen').innerHTML = 'de Kriebel, Jongslag, St-Lucas'
+}
+
+function wijkcontracten() {
+    document.getElementById('wijkcontracten').innerHTML = 'DWC, CQD, Magritte'
+}
+
+function architecten() {
+    document.getElementById('architecten').innerHTML = 'Atelier ACT, Cakri, Kiss, Onkruid, Cvek-Rigby, Urban Species, B-ILD, Bogdan-Vanbroeck, Tweestorm, Xavier, De Geyter, Landscape'
+}
