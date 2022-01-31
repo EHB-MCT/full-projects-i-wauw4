cb748d5792e1f1f4b7380f5d74c3bcfffcd9972f
Nabras Ahmad <nabras.ahmad@student.ehb.be>
2022-01-31 15:04:09 +0100

    test nbrs

--- js/index.js
+++ js/index.js
@@ -15,3 +15,17 @@ function openCity(evt, cityName) {
     evt.currentTarget.className += " active";
 }
 
+
+
+
+var password = "12345"
+
+function passcheck() {
+    if(document.getElementById('pass1').value != password) {
+        alert('Wrong Password, Try Again') 
+        return false; 
+    }
+    if(document.getElementById('pass1').value != password) {
+        alert('Correct Password,Click OK to enter Webpage' )
+    }
+}
